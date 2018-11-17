1afffeeba3d0fc63adb9ed3bb780bf8ce8978188
wellingtonapolonio <wellingtonpoloniosilva@gmail.com>
2018-11-17 21:44:27 -0200

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		ProductService ps = new ProductService();
+		
+		//double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'M');
+		double sum = ps.filteredSum(list, p -> p.getPrice() < 100.0);
+		System.out.println("Sum = "+ String.format("%.2f", sum));
+		
+		
+		
+		
+
+	}
+
+}
